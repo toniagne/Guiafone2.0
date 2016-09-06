ce67e340b54a1f77726d2083df0109f959f9ffbe
toniagne <toniagne@gmail.com>
2016-09-06 09:01:47 -0300

    ALTERAÇÕES SETEMBRO

--- js/controllers.js
+++ js/controllers.js
@@ -348,8 +348,8 @@ angular.module('starter.controllers', ['ionic'])
 
 .controller('Configuracoes', function($scope, $stateParams, Chats) {
   $scope.nomes  = "";  
-  $scope.versao = "2.0.7";
-  $scope.mes = "Agosto/2016"; 
+  $scope.versao = "2.0.8";
+  $scope.mes = "Setembro/2016"; 
 })
 
 .controller('Favoritos', function($scope, $stateParams, Chats, $localStorage, $ionicModal, $http, $window, $interval) {
